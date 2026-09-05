0754520196dbebbf5e0f1436a9b5f874a3c18a7e
kanak chouksey <kanakchouksey53@gmail.com>
2026-09-05 14:14:46 +0530

    block's border radius update

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const startgamemodal = document.querySelector('.start-game');
 const gameovermodal = document.querySelector('.game-over');
 const restartbutton = document.querySelector('.restart-btn');
 
+const highscoreElement = document.querySelector('#high-score');
+const scoreElement = document.querySelector('#score');
+const takenTime = document.querySelector('#time');
+
+let highScore = localStorage.getItem("highScore") || 0;
+let Score = 0;
+let time = `00-00`;
+highscoreElement.innerText = highScore;
+
+
 const blockheight = 50;
 const blockwidth = 50;
 
@@ -33,7 +43,7 @@ let snake = [{ x: 1, y: 3 }
 let direction = 'right';
 
 let intervalId = null;
-
+let timerIntervalId= null;
 let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
 
 
@@ -43,7 +53,7 @@ for (let row = 0; row < rows; row++) {
       const block = document.createElement('div');
       block.classList.add("block");
       board.appendChild(block);
-      block.innerText = `${row},${col}`;
+      // block.innerText = `${row},${col}`;
 
       blocks[`${row},${col}`] = block;
 
@@ -57,6 +67,7 @@ for (let row = 0; row < rows; row++) {
 // this is function to project the snake on board
 
 function renderSnake() {
+
 
    let head = null;
    blocks[`${food.x},${food.y}`].classList.add("food");
@@ -76,6 +87,9 @@ function renderSnake() {
       head = { x: snake[0].x - 1, y: snake[0].y };
    }
 
+
+   //wall collision logic
+
    if (head.x < 0 || head.y < 0 || head.y >= cols || head.x >= rows) {
 
 
@@ -83,17 +97,34 @@ function renderSnake() {
       modal.style.display = "flex";
       startgamemodal.style.display = "none";
       gameovermodal.style.display = "initial"
+
       return;
 
 
    }
 
 
+   //food consume logic
+
    if (head.x === food.x && head.y === food.y) {
       blocks[`${food.x},${food.y}`].classList.remove('food');
       food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
       blocks[`${food.x},${food.y}`].classList.add('food');
       snake.unshift(head);
+
+      Score += 10;
+      scoreElement.innerText = Score;
+      if (Score > highScore) {
+         highScore = Score;
+
+
+
+         localStorage.setItem("highScore", highScore.toString()); //in local storage data is read and store in string formate 
+
+
+
+      }
+
 
 
    }
@@ -122,6 +153,24 @@ startbutton.addEventListener("click", () => {
    intervalId = setInterval(() => {
       renderSnake();
    }, 400);
+
+   timerIntervalId= setInterval(()=>{
+      //split always retur array mean when you console time on console screen of browser it wil give `00-00` the when you console time.split("-") that mean divide time on the basis of ( - ) then it give ['00','00']
+
+      //this is called D-structuring
+      let [min,sec]= time.split('-').map(Number);
+      if(sec==59){
+         min += 1;
+         sec =0;
+      }
+      else{
+         sec += 1;
+
+      }
+      time = `${min}-${sec}`;
+      takenTime.innerText=time;
+
+   },1000);
 });
 
 
@@ -137,8 +186,13 @@ function restartgame() {
    snake.forEach(segment => {
       blocks[`${segment.x},${segment.y}`].classList.remove('fill');
    });
+   Score = 0;
+   time = `00-00`;
+   scoreElement.innerText = Score;
+   takenTime.innerText=time;
+   // highscoreElement.innerText=highScore;
 
-   direction="down";
+   direction = "down";
    modal.style.display = "none";
    snake = [{ x: 1, y: 3 }];
 
